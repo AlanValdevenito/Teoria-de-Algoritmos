5d4e31f3f99f7c7282e7498feb2a7b9d8cc522f8
Alan Ezequiel Valdevenito <avaldevenito@fi.uba.ar>
2024-09-28 13:53:52 -0300

    Resuelvo ejercicio 1 de Programacion Lineal

---
+++ EJERCICIOS/PROGRAMACION-LINEAL/EJERCICIO-1/README.md
@@ -0,0 +1,28 @@
+# Enunciado
+
+Implementar un modelo de programación lineal que resuelva el Problema de la Mochila de valor máximo (ejercicio 7 de PD).
+
+Tenemos una mochila con una capacidad W. Hay elementos a guardar, cada uno tiene un valor, y un peso que ocupa de la capacidad total. 
+Queremos maximizar el valor de lo que llevamos sin exceder la capacidad. 
+
+# Solucion
+
+Objetivo: Maximizar el valor de lo que llevamos sin exceder la capacidad W.
+
+Variables:
+- W: Capacidad
+- $w_i$: Peso del elemento i
+- $v_i$: Valor del elmento i
+- $Y_i$: Variable booleana que indica si el elemento i es guardado o no
+
+Buscamos maximizar 
+
+$$
+\sum_{i=1}^{n} v_i Y_i
+$$
+
+cumpliendose que
+
+$$
+\sum_{i=1}^{n} w_i Y_i \leq W 
+$$
---
+++ EJERCICIOS/PROGRAMACION-LINEAL/EJERCICIO-1/pl-1.py
@@ -0,0 +1,35 @@
+# Implementar un modelo de programación lineal que resuelva el Problema de la Mochila de valor máximo (ejercicio 7 de PD).
+
+from typing import List
+
+import pulp
+from pulp import LpAffineExpression as Sumatoria
+
+def mochila_variable(v: List[int], w: List[int], W: int):
+    # Lista de variables de decision binarias
+    y = []
+
+    # Se crean las variables binarias y se guardan en la lista
+    for i in range(len(v)):
+        y.append(pulp.LpVariable("y" + str(i), cat="Binary"))
+
+    # Se crea el problema de programacion lineal indicando que se trata de un problema de maximizacion
+    problem = pulp.LpProblem("Mochila", pulp.LpMaximize)
+    # Se añade una restriccion al problema
+    problem += Sumatoria([(y[i], w[i]) for i in range(len(y))]) <= W
+    # Funcion objetivo del problema
+    problem += Sumatoria([(y[i], v[i]) for i in range(len(y))])
+
+    # Se resuelve el problema de PL
+    problem.solve()
+    return list(map(lambda yi: pulp.value(yi), y))
+
+valores = [10,1,8,100,6,11,7,2,11]
+pesos = [6,1,3,100,4,2,8,7,9]
+W = 19
+y = mochila_variable(valores, pesos, W)
+
+print(y)
+
+print("Peso usado:", sum([pesos[i] * y[i] for i in range(len(y))]))
+print("Valor obtenido:", sum([valores[i] * y[i] for i in range(len(y))]))
