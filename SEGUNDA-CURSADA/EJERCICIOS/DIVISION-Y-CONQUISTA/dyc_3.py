c2df8c10d5302bc14bf32be17fca35eef403a15e
Alan Ezequiel Valdevenito <avaldevenito@fi.uba.ar>
2025-03-11 14:53:52 -0300

    Resuelvo ejercicio 3 de Division y Conquista

--- SEGUNDA-CURSADA/EJERCICIOS/DIVISION-Y-CONQUISTA/dyc_3.py
+++ SEGUNDA-CURSADA/EJERCICIOS/DIVISION-Y-CONQUISTA/dyc_3.py
@@ -5,7 +5,38 @@
 
 # Justificar el orden del algoritmo.
 
-# Aclaración: no se requiere el uso de ninguna librería de matemática que calcule la raíz cuadrada, ni de forma exacta ni aproximada.
+# Aclaración: No se requiere el uso de ninguna librería de matemática que calcule la raíz cuadrada, ni de forma exacta ni aproximada.
+
+def _parte_entera_raiz(n, inicio, fin):
+
+    medio = (inicio + fin) // 2
+
+    if (n == medio**2):
+        return medio
+    
+    if (inicio >= fin):
+        return inicio - 1
+
+    if (n > medio**2):
+        return _parte_entera_raiz(n, medio+1, fin)
+    
+    return _parte_entera_raiz(n, inicio, medio) 
 
 def parte_entera_raiz(n):
-    return 0
+    return _parte_entera_raiz(n, 0, n)
+
+# Complejidad
+
+# A: 1 (Si bien hay escritos dos llamados recursivos, en cada ejecucion se llama a uno u otro)
+# B: 2 (Se divide al problema en dos)
+# C: 0 (Costo de partir y juntar)
+
+# T(n) = A T(n/B) + O(n^C) = 2 T(n/2) + O(1)
+
+# log (1) = 0 ￫ T(n) = O(n^C log (n)) = O(n^C log (n)) = O(n^0 log (n)) = O(log(n))
+#    2                          B 
+
+assert parte_entera_raiz(0) == 0
+assert parte_entera_raiz(1) == 1
+assert parte_entera_raiz(10) == 3
+assert parte_entera_raiz(25) == 5
